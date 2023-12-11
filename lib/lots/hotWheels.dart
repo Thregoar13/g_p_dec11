bed799acfc766430bb1d5b288154dce698b654d1
Peter Sarich <113197867+Thregoar13@users.noreply.github.com>
2023-12-11 15:22:08 -0800

    no really, this time for sure

--- lib/lots/hotWheels.dart
+++ lib/lots/hotWheels.dart
@@ -35,9 +35,9 @@ class hotWheels extends StatelessWidget {
 
 
      if (MediaQuery.of(context).orientation == Orientation.portrait ){
-       rotation = 0;
-     }else{
        rotation = 1;
+     }else{
+       rotation = 0;
      }
 
      for (bool value in booleanParkingDataList) {
@@ -49,201 +49,166 @@ class hotWheels extends StatelessWidget {
       }
      }
 
-    return Scaffold(
-        resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        backgroundColor: Colors.black12,
-        title: Text('Hot Wheels Lot')
-      ),
-       //
-       body: SafeArea(
-                child: Column(
+     return Scaffold(
+         resizeToAvoidBottomInset: false,
+         appBar:
+         AppBar(backgroundColor: Colors.black12, title: Text('Hot Wheels Parking Lot')),
+         //
 
-                  children: [
-
-
-                   Container(
-                     child: FutureBuilder<ParkingLot>(
-                       future: futureParkingLot,
-                       builder: (context, snapshot) {
-                         if (snapshot.connectionState == ConnectionState.waiting) {
-                           return CircularProgressIndicator();
-                         } else if (snapshot.hasData) {
-                           parkingLot = snapshot.data;
-                           return Container(
-decoration: BoxDecoration(
-  border: Border.all(width: 4,),
-  borderRadius: BorderRadius.circular(12),
-),
-                             child: FittedBox(
-                               fit: BoxFit.fitHeight,
-                               child:RotatedBox(
-                                 quarterTurns: rotation,
-                                 child: CustomPaint(size: Size(imageWidth, imageHeight),
-                                   child: Image.network(
-                        'https://storage.googleapis.com/getparked/HotWheelsLot1.jpg',
-                        fit: BoxFit.fitHeight,
-                        
-      
-                      ),
-                                   foregroundPainter: RectanglePainter(
-                                     parkingLot!.parkingStalls,
-                                     imageWidth,
-                                     imageHeight,
-                                     booleanParkingDataList,
-                                   ),
+         body: SingleChildScrollView(
+           child: Column(
+             children: [
+               Container(
+                 child: FutureBuilder<ParkingLot>(
+                   future: futureParkingLot,
+                   builder: (context, snapshot) {
+                     if (snapshot.connectionState == ConnectionState.waiting) {
+                       return CircularProgressIndicator();
+                     } else if (snapshot.hasData) {
+                       parkingLot = snapshot.data;
+                       return Padding(
+                         padding: EdgeInsetsDirectional.fromSTEB(22, 0, 22, 0),
+                         child: Container(
+                           decoration: BoxDecoration(
+                             border: Border.all(
+                               width: 4,
+                             ),
+                             borderRadius: BorderRadius.circular(12),
+                           ),
+                           child: FittedBox(
+                             fit: BoxFit.fitHeight,
+                             child: RotatedBox(
+                               quarterTurns: rotation,
+                               child: CustomPaint(
+                                 size: Size(imageWidth, imageHeight),
+                                 child: Image.network(parkingLot!.lotURL),
+                                 // Set the size as per your image dimensions
+                                 foregroundPainter: RectanglePainter(
+                                   parkingLot!.parkingStalls,
+                                   imageWidth,
+                                   imageHeight,
+                                   booleanParkingDataList,
                                  ),
                                ),
                              ),
-                           );
-                         } else {
-                           return Text('Error: ${snapshot.error}');
-                         }
-                       },
+                           ),
+                         ),
+                       );
+                     } else {
+                       return Text('Error: ${snapshot.error}');
+                     }
+                   },
+                 ),
+               ),
+               Row(
+                 // mainAxisSize: MainAxisSize.max,
+                 mainAxisAlignment: MainAxisAlignment.center,
+                 //crossAxisAlignment: CrossAxisAlignment.end,
+                 children: [
+                   Center(
+
+                     child: Text(
+                       'Parking Lot Availability',
+                       style: TextStyle(
+                         fontSize: 22,
+                         fontFamily: 'Readex Pro',
+                         color: Colors.black,
+                         fontWeight: FontWeight.w800,
+                       ),
                      ),
                    ),
-
-
-
-
-
-
-
-
-              
-
-                          SingleChildScrollView(
-                            scrollDirection: Axis.horizontal,
-                            child: Row(
-                              mainAxisSize: MainAxisSize.max,
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: [
-                                Padding(
-                                  padding: EdgeInsetsDirectional.fromSTEB(
-                                      0, 24, 0, 8),
-                                  child: Text(
-                                    'Parking Lot Availability',
-                                    style: TextStyle(
-                                      fontSize: 22,
-                                          fontFamily: 'Readex Pro',
-                                          color: Colors.black,
-                                          fontWeight: FontWeight.w800,
-                                        ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-
-
-SingleChildScrollView(
-                            scrollDirection: Axis.horizontal,
-                            child: Row(
-                              mainAxisSize: MainAxisSize.max,
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: [
-                                Padding(
-                                  padding: EdgeInsetsDirectional.fromSTEB(
-                                      0, 20, 0, 8),
-                                  child: Text(
-                                    'Available: ',
-                                    style: TextStyle(
-                                          fontFamily: 'Readex Pro',
-                                          color: Colors.black,
-                                          fontWeight: FontWeight.w600,
-                                        ),
-                                  ),
-                                ),
-                                Padding(
-                                  padding: EdgeInsetsDirectional.fromSTEB(
-                                      0, 8, 0, 8),
-                                  child: Text(
-                                    '$countAvailable',
-                                    style: TextStyle(
-                                          fontFamily: 'Readex Pro',
-                                          color: Color(0xFF5AEF39),
-                                        ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          SingleChildScrollView(
-                            scrollDirection: Axis.horizontal,
-                            child: Row(
-                              mainAxisSize: MainAxisSize.max,
-                              children: [
-                                Padding(
-                                  padding: EdgeInsetsDirectional.fromSTEB(
-                                      0, 8, 0, 8),
-                                  child: Text(
-                                    'Occupied: ',
-                                    style: TextStyle(
-                                          fontFamily: 'Readex Pro',
-                                          color: Colors.black,
-                                          fontWeight: FontWeight.w600,
-                                        ),
-                                  ),
-                                ),
-                                Padding(
-                                  padding: EdgeInsetsDirectional.fromSTEB(
-                                      0, 8, 0, 8),
-                                  child: Text(
-                                    '$countOccupied',
-                                    style: TextStyle(
-                                          fontFamily: 'Readex Pro',
-                                          color: Color(0xFFEF393C),
-                                        ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          SingleChildScrollView(
-                            scrollDirection: Axis.horizontal,
-                            child: Row(
-                              mainAxisSize: MainAxisSize.max,
-                              children: [
-                                Padding(
-                                  padding: EdgeInsetsDirectional.fromSTEB(
-                                      0, 8, 0, 8),
-                                  child: Text(
-                                    'Handicapped:',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                          fontFamily: 'Readex Pro',
-                                          color: Colors.black,
-                                          fontWeight: FontWeight.w600,
-                                        ),
-                                  ),
-                                ),
-                                Padding(
-                                  padding: EdgeInsetsDirectional.fromSTEB(
-                                      0, 8, 0, 8),
-                                  child: Text(
-                                    ' 5',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                          fontFamily: 'Readex Pro',
-                                          color: Colors.blueGrey,
-                                          fontWeight: FontWeight.normal,
-                                        ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                  ],
-                  ),
-                
-       )
-                );
-       
-                
-       
-  }
+                 ],
+               ),
+               Row(
+                 mainAxisSize: MainAxisSize.max,
+                 mainAxisAlignment: MainAxisAlignment.center,
+                 crossAxisAlignment: CrossAxisAlignment.end,
+                 children: [
+                   Padding(
+                     padding: EdgeInsetsDirectional.fromSTEB(0, 12, 0, 8),
+                     child: Text(
+                       'Available: ',
+                       style: TextStyle(
+                         fontFamily: 'Readex Pro',
+                         color: Colors.black,
+                         fontWeight: FontWeight.w600,
+                       ),
+                     ),
+                   ),
+                   Padding(
+                     padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 4),
+                     child: Text(
+                       '$countAvailable',
+                       style: TextStyle(
+                         fontFamily: 'Readex Pro',
+                         color: Color(0xFF5AEF39),
+                       ),
+                     ),
+                   ),
+                 ],
+               ),
+               Row(
+                 mainAxisSize: MainAxisSize.max,
+                 mainAxisAlignment: MainAxisAlignment.center,
+                 children: [
+                   Padding(
+                     padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 4),
+                     child: Text(
+                       'Occupied: ',
+                       style: TextStyle(
+                         fontFamily: 'Readex Pro',
+                         color: Colors.black,
+                         fontWeight: FontWeight.w600,
+                       ),
+                     ),
+                   ),
+                   Padding(
+                     padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 4),
+                     child: Text(
+                       '$countOccupied',
+                       style: TextStyle(
+                         fontFamily: 'Readex Pro',
+                         color: Color(0xFFEF393C),
+                       ),
+                     ),
+                   ),
+                 ],
+               ),
+               Row(
+                 mainAxisSize: MainAxisSize.max,
+                 mainAxisAlignment: MainAxisAlignment.center,
+                 children: [
+                   Padding(
+                     padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 4),
+                     child: Text(
+                       'Handicapped:',
+                       textAlign: TextAlign.center,
+                       style: TextStyle(
+                         fontFamily: 'Readex Pro',
+                         color: Colors.black,
+                         fontWeight: FontWeight.w600,
+                       ),
+                     ),
+                   ),
+                   Padding(
+                     padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 4),
+                     child: Text(
+                       ' 3',
+                       textAlign: TextAlign.center,
+                       style: TextStyle(
+                         fontFamily: 'Readex Pro',
+                         color: Colors.blueGrey,
+                         fontWeight: FontWeight.normal,
+                       ),
+                     ),
+                   ),
+                 ],
+               ),
+             ],
+           ),
+         )
+     );
+   }
 }
 
 
